1758a8e0f084eb370ba7f0c5cbe37a5b52bee3ea
Mike Weigand <yrh8@cdc.gov>
2017-06-15 15:35:07 -0400

    updates to ogs scripts

--- src/ogs.binary.pl
+++ src/ogs.binary.pl
@@ -5,6 +5,7 @@ use Getopt::Long;
 #converts og matrix into binary presencen/absence. **optional add first column as PAN/og number**
 &GetOptions(	'in=s' => \my$inogs,		#
 		'ids=s' => \my$prefix,
+		'ind' => \my$ind,
 		'out=s' => \my$out);		#
 ($inogs and $out) or &HELP_MESSAGE;
 
@@ -23,6 +24,8 @@ while(my$i = <IN>){
 		my@binary=();
 
 		my$p=0;
+		my$pan='';
+		if($ind){ $pan = shift@si };
 		for (my$g=0; $g < @si; $g++){
 			if( length($si[$g]) < 2 && $si[$g] =~ /\-/){
 				push(@binary,"0");
@@ -35,6 +38,7 @@ while(my$i = <IN>){
 		my$n=sprintf("%04d", $j);
 		if($p > 1){ # && ($p < @si)){
 			if($prefix){ print OUT join("_",($prefix,$n,$p))."\t"};
+			if($ind){ print OUT $pan."\t"};
 			print OUT join("\t",@binary)."\n";
 		}
 		$j++;
@@ -53,7 +57,9 @@ sub HELP_MESSAGE { die "
 	 -out	<out>	New binary matrix.
 
    [optional]
-	 -ids	<prefix>	Add first column with og identifiers (eg 'PAN').
+	 -ids	<pre>	Add first column with og identifiers using given prefix (eg 'PAN').
+	 -ind		Input ogs matrix already has indices in first column (Default: 'off').
+
 
    [dependencies]
 
--- src/ogs.submatrix.pl
+++ src/ogs.submatrix.pl
@@ -6,17 +6,32 @@ use File::Basename;
 
 &GetOptions(	'in=s' => \my$inogs,		#
 		'list=s' => \my$list,
+		'ids=s' => \my$prefix,
+		'min=s' => \my$min,
+		'ind' => \my$ind,
+		'max=s' => \my$max,
 		'out=s' => \my$out);		#
-($inogs and $list and $out) or &HELP_MESSAGE;
+($inogs and $out) or &HELP_MESSAGE;
+($list or $prefix or ($min and $max)) or &HELP_MESSAGE;
+if($list and ($min or $max)){ &HELP_MESSAGE };
+
+
+if($prefix){
+	print "\nWARNING: parameter '-ids' overrides all other options. A column of og identifiers have been added but no filtering was performed.\n\n";
+}
 
 my@header=();
 my%genomes=();
-open LIST, "$list";
-while(my$l = <LIST>){
-	chomp$l;
-	$genomes{ $l } = 1;
+my$j=0;
+
+if($list){
+	open LIST, "$list";
+	while(my$l = <LIST>){
+		chomp$l;
+		$genomes{ $l } = 1;
+	}
+	close LIST;
 }
-close LIST;
 
 open OGS, "$inogs";
 open OUT, ">$out";
@@ -24,39 +39,87 @@ while(my$i = <OGS>){
 	chomp$i;
 	my@si=split("\t",$i);
 	if( scalar(@header) < 1){
-		for(my$c = 0; $c < @si; $c++){
-			if(exists( $genomes{ $si[$c] })){
-				push(@header,$c);
-				delete $genomes{ $si[$c]};
+		if($list){
+			for(my$c = 0; $c < @si; $c++){
+				if(exists( $genomes{ $si[$c] })){
+					push(@header,$c);
+					delete $genomes{ $si[$c]};
+				}
 			}
+			print OUT join("\t",@si[@header])."\n";
+			print "Genomes matched: ".scalar(@header)."\n";
+			print "Genomes missing: ".scalar(keys%genomes)."\n";
+
+		}else{
+			@header = @si;
+			if($prefix){ print OUT "\t" };
+			print OUT join("\t",@si)."\n";
 		}
-		print OUT join("\t",@si[@header])."\n";
-		print "Genomes matched: ".scalar(@header)."\n";
-		print "Genomes missing: ".scalar(keys%genomes)."\n";
-		if(scalar(keys%genomes) > 0){
+
+		if($list and scalar(keys%genomes) > 0){
 			print "\t\t\t".join("\n\t\t\t",(sort(keys%genomes)))."\n";
 		}
+
 	}else{
-		print OUT join("\t",@si[@header])."\n";
+		if($prefix){
+			my$p = &countogs( \@si );
+			my$n=sprintf("%04d", $j);
+			if($p > 1){
+				print OUT join("_",($prefix,$n,$p))."\t";
+				print OUT join("\t",@si)."\n";
+			}
+			$j++;
+			next;
+
+		}elsif($list){
+			print OUT join("\t",@si[@header])."\n";
+		}else{
+			my$pan='';
+			if($ind){ $pan = shift@si };
+			my$present = &countogs( \@si );
+			if(($present >= $min) && ($present <= $max)){
+				if($ind){ print OUT $pan."\t" };
+				print OUT join("\t",@si)."\n";
+			}
+		}
 	}
+
 }
 close OUT;
 
 
 ################################
+sub countogs {
+	my@in = @{$_[0]};
+	my$zero = 0;
+	foreach my$i (@in){
+		if($i =~ /^\d+$/){
+			if($i == 0){ $zero++};
+		}elsif($i eq '-'){
+			$zero++;
+		}
+	}
+	return( scalar@in - $zero );
+}
+
+
 sub HELP_MESSAGE { die "
 .Description:
-   Takes ogs matrix and makes new sub-matrix from list of genomes.
+   Takes ogs matrix and makes new sub-matrix based on: [1] list of genomes, or [2] frequency min and/or max.
 
-.Usage: $0 -in [ogs] -out [out] -list [list]
+.Usage: $0 -in [ogs] -out [out] -list [list] -min [int] -max [int]
 
    [mandatory]
 	 -in	<ogs>	Tab-separated matrix of ogs, like from ogs.mcl.rb.
 	 -out	<out>	New sub-matrix of ogs in list.
-	 -list	<list>	List of genomes/names to match in header of ogs matrix.
 
    [optional]
-	 -r	Reverse list, returns sub-matrix of genomes NOT in list.
+	 -list	<list>	List of genomes/names to match in header of ogs matrix.
+	 -r		Reverse list, returns sub-matrix of genomes NOT in list. (NOT FUNCTIONAL)
+	 -min	<int>	Only output ogs present in at _least_ this many genomes.
+	 -max	<max>	Only output ogs present in at _most_ this many genomes.
+	 -ind		Input ogs matrix has indices in first column (Default: 'off').
+	 -ids	<pre>	Add first column with og identifiers using given prefix, OVERRIDES ALL OTHER OPTIONS (eg 'PAN').
 
 
    [dependencies]
