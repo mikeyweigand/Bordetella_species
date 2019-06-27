06e02eb37c6c8fbd5f4afa64279da4eea4e6be77
Mike Weigand <yrh8@cdc.gov>
2019-06-27 15:53:03 -0400

    updates kmer-frequency pipeline

--- pipelines/kmer-frequency.sh
+++ pipelines/kmer-frequency.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 
+if [[ "$1" == "" || "$1" == "-h" ]] ; then
+   echo "
+   This script will run jellyfish on a directory full of genome sequence fasta files and output files to './results/jellyfish/YYYYMMDD/'
+	 Path to the jellyfish executable is hard-coded so be sure to set it accordingly.
+	 Final summary table depends on 'Table.merge.pl' (Enveomics)
+
+   Usage: ./pipelines/kmer-frequency.sh [k] [folder]
+
+   folder       Path to the directory containing the genome files (*.fasta)
+   k            Your favorite kmer length in bp (eg. 15)
+
+
+   " >&2 ;
+   exit 1 ;
+fi ;
+
+#where is your jellyfish?
+myjelly="/home/yrh8/Tools/jellyfish-2.2.6/bin"
+
 #set values
 k=15;
+k=$1;
 
 #count kmers using jellyfish
 now=$(date +%Y%m%d);
@@ -10,25 +30,25 @@ if [ ! -d "results/jellyfish/$now" ];then
 	mkdir results/jellyfish/$now;
 fi
 date > results/jellyfish/$now/$log.log;
-echo ~/Tools/jellyfish-2.2.6/bin/jellyfish >> results/jellyfish/$now/$log.log;
-~/Tools/jellyfish-2.2.6/bin/jellyfish --version >> results/jellyfish/$now/$log.log;
+echo $myjelly/jellyfish >> results/jellyfish/$now/$log.log;
+$myjelly/jellyfish --version >> results/jellyfish/$now/$log.log;
 echo "k="$k | tee -a results/jellyfish/$now/$log.log;
 
-for i in $(ls data/genomes/fasta/*fasta);
+for i in $(ls $2/*fasta);
 do
 	echo $i | tee -a results/jellyfish/$now/$log.log;
-	o=$(basename $i _Illumina_final.fasta);
+	o=$(basename $i .fasta);
 
-	~/Tools/jellyfish-2.2.6/bin/jellyfish bc -m $k -s 5M -t 16 \
+	$myjelly/jellyfish bc -m $k -s 5M -t 16 \
 	-o results/jellyfish/$now/$o\-$k\mer.bc $i;
-	~/Tools/jellyfish-2.2.6/bin/jellyfish count -m $k -s 5M -t 16 \
+	$myjelly/jellyfish count -m $k -s 5M -t 16 \
 	--bc results/jellyfish/$now/$o\-$k\mer.bc $i \
 	-o results/jellyfish/$now/$o\-$k\mer.jf;
 
-	~/Tools/jellyfish-2.2.6/bin/jellyfish histo results/jellyfish/$now/$o\-$k\mer.jf \
+	$myjelly/jellyfish histo results/jellyfish/$now/$o\-$k\mer.jf \
 	-o results/jellyfish/$now/$o\-$k\mer.hist;
 
-	for j in $(~/Tools/jellyfish-2.2.6/bin/jellyfish dump -c -t results/jellyfish/$now/$o\-$k\mer.jf | cut -f2); \
+	for j in $($myjelly/jellyfish dump -c -t results/jellyfish/$now/$o\-$k\mer.jf | cut -f2); \
 	do echo -e $o"\t"$j; done > results/jellyfish/$now/$o\-$k\mer.dump;
 
 done
@@ -37,17 +57,9 @@ if [ ! -d "results/jellyfish/$now/hist" ];then
 	mkdir results/jellyfish/$now/hist;
 fi
 
-for i in $(ls B*hist | cut -f1 -d"_" | uniq);
+for i in $(ls results/jellyfish/$now/*.hist | cut -f1 -d"_" | uniq);
 do
 	n=$(basename $i);
 	Table.merge.pl -i " " results/jellyfish/$now/$n\_*.hist | sort -n > results/jellyfish/$now/hist/$n\-$k\mer.hist;
 
 done
-
-
-
-#ggplot
-#cat Bb_I328-15mer.dump Bhi_F582-15mer.dump Bho_F613-15mer.dump Bp_J021-15mer.dump Bpp_H904-15mer.dump Bsp_H567-15mer.dump Bt_F581-15mer.dump > dump3;
-#dump03 = read.table("dump3");
-#qplot( V2, data = dump03, geom = "density" , fill=V1, alpha=.3, ylim=c(0,0.02)) + theme_bw( ) + scale_x_continuous(breaks=seq(0,150,10))
-#qplot( V2, data = dump03, geom = "density" , fill=V1, alpha=.2) + scale_x_continuous(breaks=seq(0,150,10),limits=c(10,150)) + coord_cartesian(ylim=c(0,0.4));
